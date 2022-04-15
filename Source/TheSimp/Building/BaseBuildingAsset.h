395a55f1c31af45ee9d4bac0d7b261d47d805a00
Boss <bossnathakorn@gmail.com>
2022-04-15 19:36:34 +0700

    fix assets

---
+++ Source/TheSimp/Building/BaseBuildingAsset.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseBuildingAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESIMP_API UBaseBuildingAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Mesh)
+	TSoftObjectPtr<UStaticMesh> Mesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Details)
+	FString Name;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Details)
+	FString Description;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Details)
+	float Price;
+};
